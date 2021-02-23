0da9976f3cf8a632c2945f5571d08e48d72dd206
Nur A Alam <nuraalam376@gmail.com>
2021-02-23 09:31:20 +0600

    practiced typescript types

--- Typescript basic/output/script.js
+++ Typescript basic/output/script.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("Hello TypeScript!!");
+var playerName;
+playerName = "Abc";
+console.log(playerName);
+playerName = 34;
+var players = ["Abc", 34, {}];
+players.push(34);
+players.push({
+    name: "Def",
+    age: 25,
+});
+var player = {
+    name: "'Ghi",
+    age: 25,
+    country: "",
+};
+player.country = "Bd";
+function multiply(a, b) {
+    return a + b;
+}
+console.log(multiply(3, 4));
--- Typescript basic/src/script.ts
+++ Typescript basic/src/script.ts
@@ -1,1 +1,30 @@
-console.log("Hello TypeScript!!");
+let playerName;
+
+playerName = "Abc";
+
+console.log(playerName);
+
+playerName = 34;
+
+let players = ["Abc", 34, {}];
+
+players.push(34);
+
+players.push({
+  name: "Def",
+  age: 25,
+});
+
+let player = {
+  name: "'Ghi",
+  age: 25,
+  country: "",
+};
+
+player.country = "Bd";
+
+function multiply(a: number, b: number) {
+  return a + b;
+}
+
+console.log(multiply(3, 4));
