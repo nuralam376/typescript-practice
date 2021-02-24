5c1eef1b2bf17d7464480cdf6d0114967165bf74
Nur A Alam <nuraalam376@gmail.com>
2021-02-24 09:30:53 +0600

    practiced typescript explicit types

--- Typescript basic/src/script.ts
+++ Typescript basic/src/script.ts
@@ -1,30 +1,60 @@
-let playerName;
+// let playerName;
 
-playerName = "Abc";
+// playerName = "Abc";
 
-console.log(playerName);
+// console.log(playerName);
 
-playerName = 34;
+// playerName = 34;
 
-let players = ["Abc", 34, {}];
+// let players = ["Abc", 34, {}];
 
-players.push(34);
+// players.push(34);
 
-players.push({
-  name: "Def",
-  age: 25,
-});
+// players.push({
+//   name: "Def",
+//   age: 25,
+// });
 
-let player = {
-  name: "'Ghi",
-  age: 25,
-  country: "",
+// let player = {
+//   name: "'Ghi",
+//   age: 25,
+//   country: "",
+// };
+
+// player.country = "Bd";
+
+// function multiply(a: number, b: number) {
+//   return a + b;
+// }
+
+// console.log(multiply(3, 4));
+
+let a: string;
+
+a = "abc";
+
+let b: number;
+
+// b = "abc";
+b = 5;
+
+let array: string[] = [];
+
+array.push("s");
+// array.push(5);
+
+let arr: (number | string)[] = [];
+
+arr.push("name");
+arr.push(5);
+// arr.push(false);
+
+let obj: {
+  name: string;
 };
 
-player.country = "Bd";
+// obj.name = "as";
 
-function multiply(a: number, b: number) {
-  return a + b;
-}
-
-console.log(multiply(3, 4));
+obj = {
+  name: "abc",
+};
