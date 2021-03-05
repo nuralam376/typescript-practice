85796fdc2108c019fad9ed8b10ed3488e9e70422
Nur A Alam <nuraalam376@gmail.com>
2021-03-05 09:07:36 +0600

    practiced typescript typed

--- typescript-tutorial/sandbox.js
+++ typescript-tutorial/sandbox.js
@@ -1,7 +1,66 @@
-var character = "Abc";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// let character = "Abc";
+// let age = 30;
+// // character = 30;
+// // age = "as";
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(circ(12));
+// let arrays = ["abc", "def", "ghi"];
+// arrays.push("def");
+// // arrays.push(23);
+// arrays[0] = "as";
+// // arrays[1] = 25;
+// // arrays = "gekko";
+// let numbers = [23, 2, 3];
+// numbers.push(23);
+// // numbers.push("qw");
+// numbers = [];
+// // numbers = "test";
+// let mixed = ["ghj", 12, "av", true];
+// mixed.push("as");
+// mixed.push(12);
+// mixed.push(false);
+// mixed = [];
+// // mixed = "test";
+// let person = {
+//   name: "abc",
+//   age: 25,
+//   country: "bd",
+// };
+// person.name = "def";
+// // person.age = "23";
+// // person.skills = ["lan"];
+// person = {
+//   name: "def",
+//   age: 45,
+//   country: "eng",
+//   // skills: ["a"],
+// };
+var character;
+var age;
+var isLoggedIn;
+character = "abc";
+age = 23;
+isLoggedIn = false;
+var arrays = [];
+arrays[0] = "21";
+arrays.push("21");
+// arrays[3] = false;
+var mixed = [];
+mixed.push(21);
+mixed.push("as");
+// mixed.push(false);
+var person;
+// person = "as";
+person = [];
+// person;
+var people;
+people = {
+    name: "Abc",
+    age: 21,
+    country: "bnd"
+};
+var uuid;
+uuid = 23;
+uuid = "bd";
--- typescript-tutorial/sandbox.ts
+++ typescript-tutorial/sandbox.ts
@@ -1,11 +1,100 @@
-const character = "Abc";
+// let character = "Abc";
 
-console.log(character);
+// let age = 30;
 
-const inputs = document.querySelectorAll("input");
+// // character = 30;
 
-console.log(inputs);
+// // age = "as";
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+
+// console.log(circ(12));
+
+// let arrays = ["abc", "def", "ghi"];
+
+// arrays.push("def");
+
+// // arrays.push(23);
+// arrays[0] = "as";
+// // arrays[1] = 25;
+
+// // arrays = "gekko";
+
+// let numbers = [23, 2, 3];
+
+// numbers.push(23);
+// // numbers.push("qw");
+// numbers = [];
+// // numbers = "test";
+
+// let mixed = ["ghj", 12, "av", true];
+
+// mixed.push("as");
+// mixed.push(12);
+// mixed.push(false);
+
+// mixed = [];
+// // mixed = "test";
+
+// let person = {
+//   name: "abc",
+//   age: 25,
+//   country: "bd",
+// };
+
+// person.name = "def";
+// // person.age = "23";
+// // person.skills = ["lan"];
+
+// person = {
+//   name: "def",
+//   age: 45,
+//   country: "eng",
+//   // skills: ["a"],
+// };
+
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+character = "abc";
+age = 23;
+isLoggedIn = false;
+
+let arrays: string[] = [];
+
+arrays[0] = "21";
+arrays.push("21");
+// arrays[3] = false;
+
+let mixed: (number | string)[] = [];
+mixed.push(21);
+mixed.push("as");
+// mixed.push(false);
+
+let person: object;
+
+// person = "as";
+
+person = [];
+
+// person;
+
+let people: {
+  name: string;
+  age: number;
+  country: string;
+};
+
+people = {
+  name: "Abc",
+  age: 21,
+  country: "bnd",
+};
+
+let uuid: string | number;
+
+uuid = 23;
+uuid = "bd";
