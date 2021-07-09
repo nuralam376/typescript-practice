67f8000c3e226dd2d332d8908c5ef4fa66798979
Nur A Alam <nuraalam376@gmail.com>
2021-07-09 22:11:37 +0600

    practiced typescript types

--- Typescript-advanced/app.ts
+++ Typescript-advanced/app.ts
@@ -2,6 +2,26 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const Id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(Id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    Todo Id - ${id}
+    Todo title - ${title}
+    Todo completed - ${completed}
+  `);
+};
---
+++ Typescript-advanced/types.ts
@@ -0,0 +1,14 @@
+const date = new Date();
+
+console.log(date.getMonth());
+
+const person = {
+  age: 20,
+};
+
+// person.name = 'Abc';
+
+class Color = {};
+
+const red = new Color();
+// red.fill = "";
